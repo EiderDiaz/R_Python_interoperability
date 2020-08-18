6cd09f3754a7fdea6a0870e7b2894664fc2521a2
RebecaCanales <marthar.canales@gmail.com>
2020-08-18 11:22:22 -0500

    Update __init__.py

--- tadpole_algorithms/models/__init__.py
+++ tadpole_algorithms/models/__init__.py
@@ -2,5 +2,6 @@ from .emceb import EMCEB
 from .benchmark_svm import BenchmarkSVM
 from .benchmark_last_visit import BenchmarkLastVisit
 from .benchmark_SVM_R import BenchmarkSVM_R
+from .bechmark_model import BenchmarkModel
 #from .benchmark_caret import BenchmarkCaret
 
